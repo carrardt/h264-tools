27bb9d9912315d0035b574838dd6849e6cab87b7
Thierry Carrard <thierry@carrard.org>
2014-10-30 07:37:08 +0100

    SPS analysis, try to find how to detect MVC streams

--- tools/naluparser.c
+++ tools/naluparser.c
@@ -21,6 +21,23 @@ enum  	SLICE_type {
  
 enum  	frame_type { FRAME = 'F', FIELD_TOP = 'T', FIELD_BOTTOM = 'B' };
 
+//AVC Profile IDC definitions
+typedef enum {
+  FREXT_CAVLC444 = 44,       //!< YUV 4:4:4/14 "CAVLC 4:4:4"
+  BASELINE       = 66,       //!< YUV 4:2:0/8  "Baseline"
+  MAIN           = 77,       //!< YUV 4:2:0/8  "Main"
+  EXTENDED       = 88,       //!< YUV 4:2:0/8  "Extended"
+  FREXT_HP       = 100,      //!< YUV 4:2:0/8  "High"
+  FREXT_Hi10P    = 110,      //!< YUV 4:2:0/10 "High 10"
+  FREXT_Hi422    = 122,      //!< YUV 4:2:2/10 "High 4:2:2"
+  FREXT_Hi444    = 244,      //!< YUV 4:4:4/14 "High 4:4:4"
+  MVC_HIGH       = 118,      //!< YUV 4:2:0/8  "Multiview High"
+  STEREO_HIGH    = 128       //!< YUV 4:2:0/8  "Stereo High"
+} ProfileIDC;
+
+
+typedef unsigned char byte;
+
 static const unsigned char AccessUnitDelimiterSC[6] = { 0x00, 0x00, 0x00, 0x01, 0x09, 0x10 };
 static const unsigned char AccessUnitDelimiterLen[6] = { 0x00, 0x00, 0x00, 0x02, 0x09, 0x10 };
 static const unsigned char StartCode[4] = {0x00, 0x00, 0x00, 0x01};
@@ -36,6 +53,9 @@ struct ParserCtx
 	size_t naluLen, maxNaluSize, naluCount, maxNALUs, naluTotalBytes;
 	int naluType,zeros;
 	int insertAUD,verbose,writeOutput,naluStartCode;
+	int sps_id_range[2];
+	int profile_idc_range[2];
+	int num_ref_frames_range[2];
 	unsigned char sc[4];
 };
 
@@ -70,7 +90,6 @@ static inline size_t readInput(struct ParserCtx* ctx, unsigned char* buf, size_t
 	return count;
 }
 
-
 struct ParserCtx* allocContext()
 {
 	struct ParserCtx* ctx = (struct ParserCtx*)calloc(1,sizeof(struct ParserCtx));
@@ -78,6 +97,12 @@ struct ParserCtx* allocContext()
 	ctx->writeOutput = 1;
 	ctx->naluStartCode = 1;
 	ctx->maxNaluSize = 1<<20;
+	ctx->sps_id_range[0]=255;
+	ctx->sps_id_range[1]=-1;
+	ctx->profile_idc_range[0]=255;
+	ctx->profile_idc_range[1]=-1;
+	ctx->num_ref_frames_range[0]=255;
+	ctx->num_ref_frames_range[1]=-1;
 	ctx->buf = (unsigned char*) calloc(1,ctx->maxNaluSize);
 	return ctx;
 }
@@ -89,8 +114,79 @@ void freeContext(struct ParserCtx* ctx)
 	free(ctx); 
 }
 
+
+static inline int linfo_ue(int len, int info)
+{
+  //assert ((len >> 1) < 32);
+  return (int) (((unsigned int) 1 << (len >> 1)) + (unsigned int) (info) - 1);
+}
+
+static inline int GetVLCSymbol (byte buffer[],int totbitoffset,int *info, int bytecount)
+{
+  long byteoffset = (totbitoffset >> 3);         // byte from start of buffer
+  int  bitoffset  = (7 - (totbitoffset & 0x07)); // bit from start of byte
+  int  bitcounter = 1;
+  int  len        = 0;
+  byte *cur_byte  = &(buffer[byteoffset]);
+  int  ctr_bit    = ((*cur_byte) >> (bitoffset)) & 0x01;  // control bit for current bit posision
+
+  while (ctr_bit == 0)
+  {                 // find leading 1 bit
+    len++;
+    bitcounter++;
+    bitoffset--;
+    bitoffset &= 0x07;
+    cur_byte  += (bitoffset == 7);
+    byteoffset+= (bitoffset == 7);
+    ctr_bit    = ((*cur_byte) >> (bitoffset)) & 0x01;
+  }
+
+  if (byteoffset + ((len + 7) >> 3) > bytecount)
+    return -1;
+  else
+  {
+    // make infoword
+    int inf = 0;                          // shortest possible code is 1, then info is always 0    
+
+    while (len--)
+    {
+      bitoffset --;
+      bitoffset &= 0x07;
+      cur_byte  += (bitoffset == 7);
+      bitcounter++;
+      inf <<= 1;
+      inf |= ((*cur_byte) >> (bitoffset)) & 0x01;
+    }
+
+    *info = inf;
+    return bitcounter;           // return absolute offset in bit from start of frame
+  }
+}
+
+#define INSERT_VALUE_TO_RANGE(x,r) do { if(x<ctx->r[0]) ctx->r[0]=x; if(x>ctx->r[1]) ctx->r[1]=x; }while(0)
+
 #define NALU_STATS(ctx) do { \
 ++ ctx->typeStats[ctx->naluType&31]; \
+if( ctx->naluType == SPS ) { \
+	int bitoffset=32,len=0,info=0,sps_id=-1,profile_idc=-1,num_ref_frames=-1, vui_prameters_present_flag=-1; \
+	profile_idc = ctx->buf[1]; \
+	INSERT_VALUE_TO_RANGE(profile_idc,profile_idc_range); \
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	sps_id = linfo_ue(len,info); \
+	INSERT_VALUE_TO_RANGE(sps_id,sps_id_range); \
+	bitoffset=32+len; \
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	num_ref_frames = linfo_ue(len,info); \
+	INSERT_VALUE_TO_RANGE(num_ref_frames,num_ref_frames_range);\
+	++bitoffset;\
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	bitoffset += ( len = GetVLCSymbol(ctx->buf,bitoffset,&info,ctx->naluLen-(bitoffset/8)) ); \
+	bitoffset += 3; \
+	vui_prameters_present_flag = ( ctx->buf[bitoffset>>3] & (7-(bitoffset&0x07)) ) != 0; \
+} \
 if( (ctx->verbose==1 && (ctx->naluCount%256)==0 ) || ctx->verbose>=2 ) \
 { \
     fprintf(stderr,"NALUs: %ld, parsed %ld Mb, last NALU: size=%ld, type=%d" \
@@ -101,7 +197,7 @@ if( (ctx->verbose==1 && (ctx->naluCount%256)==0 ) || ctx->verbose>=2 ) \
 
 #define NALU_FINAL_SATS(ctx) do {\
 int _c; \
-fprintf(stderr,"\nNALUs count: %ld, Total bytes %ld\n",ctx->naluCount,ctx->naluTotalBytes); \
+fprintf(stderr,"\n%ld NALUs, %ld bytes, sps [%d:%d], profile [%d:%d], ref_frames [%d:%d]\n",ctx->naluCount,ctx->naluTotalBytes,ctx->sps_id_range[0],ctx->sps_id_range[1],ctx->profile_idc_range[0],ctx->profile_idc_range[1],ctx->num_ref_frames_range[0],ctx->num_ref_frames_range[1]); \
 for(_c=0;_c<32;_c++) { if(ctx->typeStats[_c]>0) fprintf(stderr,"NALU type %d count : %ld\n",_c,ctx->typeStats[_c]); } \
 } while(0)
 
